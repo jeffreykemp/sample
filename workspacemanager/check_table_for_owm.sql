71e164c0e1b4011ad9b22a94892702082147d8cd
Jeffrey Kemp <jeffrey.kemp@jk64.com>
2018-03-12 10:03:47 +0800

    Create check_table_for_owm.sql

---
+++ workspacemanager/check_table_for_owm.sql
@@ -0,0 +1,19 @@
+prompt check_table_for_owm.sql
+
+prompt Report any column names that are too long
+select table_name, column_name
+from user_tab_columns
+where table_name = :TABLE_NAME
+and (length(column_name) > 28 or length(table_name) > 25);
+
+prompt Report any index names that are too long (to support workspace manager table alters)
+select table_name, index_name
+from user_indexes
+where table_name = :TABLE_NAME
+and length(index_name) > 26;
+
+prompt Report any constraint names that are too long (to support workspace manager table alters)
+select table_name, constraint_name
+from user_constraints
+where table_name = :TABLE_NAME
+and length(constraint_name) > 26;
