235cc52915f25955dbbf3b9d92c7e81bd40d8f88
Jeffrey Kemp <jeffrey.kemp@jk64.com>
2020-04-27 10:18:01 +0800

    Font Awesome 5 Free

---
+++ fontawesome/create_table_fonticons.sql
@@ -0,0 +1,6 @@
+create table fonticons (
+    library                        varchar2(100) not null,
+    class                          varchar2(10) not null,
+    icon                           varchar2(100) not null,
+    constraint fonticons_pk primary key (library, prefix, icon)
+);
