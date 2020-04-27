2511de24240ad39253a1b4612645444d4a9ccd6a
Jeffrey Kemp <jeffrey.kemp@jk64.com>
2020-04-27 10:46:44 +0800

    Update create_table_fonticons.sql

--- fontawesome/create_table_fonticons.sql
+++ fontawesome/create_table_fonticons.sql
@@ -2,5 +2,5 @@ create table fonticons (
     library                        varchar2(100) not null,
     class                          varchar2(10) not null,
     icon                           varchar2(100) not null,
-    constraint fonticons_pk primary key (library, prefix, icon)
+    constraint fonticons_pk primary key (library, class, icon)
 );
