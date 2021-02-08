45a9026fdc3beae9c8426016a3e1f1a81dd29ee9
Jeffrey Kemp <jeffrey.kemp@jk64.com>
2021-02-08 15:19:31 +0800

    Update str_array_pkg.sql

--- str_array_pkg/str_array_pkg.sql
+++ str_array_pkg/str_array_pkg.sql
@@ -12,19 +12,10 @@ type map_type is table of varchar2(32767) index by varchar2(255);
 c_crlf constant varchar2(2) := chr(13) || chr(10);
 
 -------------------------------------------------------------------
--- Delete a range of lines from the array
--------------------------------------------------------------------
-procedure del (
-    p_lines_io in out array_type,
-    p_from_idx in binary_integer := null,
-    p_to_idx   in binary_integer := null
-);
-
--------------------------------------------------------------------
 -- Insert an array into another array at a particular position
 -------------------------------------------------------------------
 procedure ins (
-    p_lines_io in out array_type,
+    p_lines_io in out nocopy array_type,
     p_new      in array_type,
     p_at_idx   in binary_integer
 );
@@ -33,7 +24,7 @@ procedure ins (
 -- Insert all elements of the array on the end of the first
 -------------------------------------------------------------------
 procedure append (
-    p_lines_io in out array_type,
+    p_lines_io in out nocopy array_type,
     p_new      in array_type
 );
 
@@ -41,7 +32,7 @@ procedure append (
 -- Insert all elements of the array at the start of the first
 -------------------------------------------------------------------
 procedure prepend (
-    p_lines_io in out array_type,
+    p_lines_io in out nocopy array_type,
     p_new      in array_type
 );
 
@@ -50,14 +41,44 @@ procedure prepend (
 -- given range
 -------------------------------------------------------------------
 procedure upd (
-    p_lines_io in out array_type,
+    p_lines_io in out nocopy array_type,
     p_new      in array_type,
     p_from_idx in binary_integer,
     p_to_idx   in binary_integer := null -- default is p_from_idx
 );
 
 -------------------------------------------------------------------
--- Create an array from a range of strings within another
+-- Delete a range of lines from the array
+-------------------------------------------------------------------
+procedure del (
+    p_lines_io in out nocopy array_type,
+    p_from_idx in binary_integer := null,
+    p_to_idx   in binary_integer := null
+);
+
+-------------------------------------------------------------------
+-- Move one line from one location to another.
+-- Raises exception if the target location already has data.
+-------------------------------------------------------------------
+procedure move (
+    p_lines_io in out nocopy array_type,
+    p_src_idx  in binary_integer,
+    p_tgt_idx  in binary_integer
+);
+
+-------------------------------------------------------------------
+-- Shift all strings between the given range by the given offset
+-- Does nothing if p_offset is null or zero
+-------------------------------------------------------------------
+procedure shift (
+    p_lines_io in out nocopy array_type,
+    p_from_idx in binary_integer := null, -- default is from start of array
+    p_to_idx   in binary_integer := null, -- default is to end of array
+    p_offset   in binary_integer
+);
+
+-------------------------------------------------------------------
+-- Get a range of strings from within an array
 -- If p_preserve_indices = true, the array indices are preserved
 -- If p_preserve_indices = false, the array indices will start from
 -- 1 (or p_first_idx) up to number of lines.
@@ -71,12 +92,23 @@ function slice (
 ) return array_type;
 
 -------------------------------------------------------------------
+-- Count the number of lines between the given indices (inclusive)
+-------------------------------------------------------------------
+function count_between (
+    p_lines    in array_type,
+    p_from_idx in binary_integer := null,
+    p_to_idx   in binary_integer := null
+) return integer;
+
+-------------------------------------------------------------------
 -- Perform the substitution on all the lines in the array
 -------------------------------------------------------------------
 procedure replace_all (
     p_lines_io in out nocopy array_type,
     p_old      in varchar2,
-    p_new      in varchar2
+    p_new      in varchar2,
+    p_from_idx in binary_integer := null,
+    p_to_idx   in binary_integer := null
 );
 
 -------------------------------------------------------------------
@@ -84,18 +116,20 @@ procedure replace_all (
 -------------------------------------------------------------------
 procedure replace_all (
     p_lines_io in out nocopy array_type,
-    p_str_map  in map_type
+    p_str_map  in map_type,
+    p_from_idx in binary_integer := null,
+    p_to_idx   in binary_integer := null
 );
 
 -------------------------------------------------------------------
--- Renumber the array indices so it starts from 1 (or p_first_idx)
--- with no gaps.
--- Warning: does not handle array indices < 1
+-- Move the array indices according to the new starting index
+-- and increment. May be used to remove gaps.
 -------------------------------------------------------------------
-procedure remove_gaps (
-    p_lines_io  in out nocopy array_type,
-    p_first_idx in binary_integer := 1
-);
+function renumber (
+    p_lines_io  in array_type,
+    p_start_idx in binary_integer := 1,
+    p_increment in integer        := 1
+) return array_type;
 
 -------------------------------------------------------------------
 -- Compress consecutive blank lines
